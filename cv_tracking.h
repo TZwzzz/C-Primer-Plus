899221a45718389454e2bdf4662041d8f16d8245
TZwzzz <120455615+TZwzzz@users.noreply.github.com>
2023-03-29 10:37:37 +0800

    修改打印update为可选项

--- cv_tracking.h
+++ cv_tracking.h
@@ -7,8 +7,9 @@
 #define KCF     0
 #define MOSSE   1
 
-#define PRINT_ROI   0
-#define DRAW_ROI    0
+#define PRINT_ROI     0
+#define DRAW_ROI      0
+#define PRINT_UPDATE  0
 
 
 cv::Rect2d cv_tracking(void *src_mb,int f_width,int f_height,
