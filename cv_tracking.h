9e151afc3dbd2b212f4ded44869755025a3e5c3c
TZwzzz <120455615+TZwzzz@users.noreply.github.com>
2023-03-30 16:25:13 +0800

    修复开关变量不起作用，增加log打印函数

--- cv_tracking.h
+++ cv_tracking.h
@@ -1,5 +1,5 @@
 #ifndef KCF_TRACKING_H_
-#define KCF_TRACKING_H
+#define KCF_TRACKING_H_
 
 #include "opencv2/opencv.hpp"
 #include "opencv2/tracking.hpp"
@@ -7,12 +7,8 @@
 #define KCF     0
 #define MOSSE   1
 
-#define PRINT_ROI     0
-#define DRAW_ROI      0
-#define PRINT_UPDATE  0
-
-
 cv::Rect2d cv_tracking(void *src_mb,int f_width,int f_height,
                   cv::Rect2d roi,bool *tracking_init,int tracking_algo = KCF);
+void cv_tracking_log(bool print_roi = false,bool print_update = false,bool draw_result_rect = false);
 
 #endif
